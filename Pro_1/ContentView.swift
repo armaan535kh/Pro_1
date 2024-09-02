c8e3655f8e34ecfbd41e22b702586f731c8bdd7c
armaan535kh <kh.armaan535@gmail.com>
2024-09-02 05:03:23 -0700

    Completed

--- Pro_1/ContentView.swift
+++ Pro_1/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            
+                List {
+                    ForEach(0..<20) { _ in
+                        episodeCell
+                    }
+                }
+                .safeAreaInset(edge: .bottom) {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.pink.gradient.opacity(0.8))
+                        .frame(maxWidth: .infinity, maxHeight: 70)
+                        .padding()
+                }
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.pink.gradient.opacity(0.8))
+                    .frame(maxWidth: .infinity, maxHeight: 70)
+                    .padding()
+            
+            
+            
         }
-        .padding()
+        
+        
+        
+    }
+    
+    var episodeCell: some View {
+        HStack(alignment: .top, spacing: 12) {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 80, height: 80)
+                .foregroundStyle(.indigo.gradient.opacity(0.5))
+            
+            VStack(alignment: .leading) {
+                Text("Episode Title")
+                    .font(.headline)
+                
+                Text("Here is the short discription for latest episode.")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+            
+        }
     }
 }
 
