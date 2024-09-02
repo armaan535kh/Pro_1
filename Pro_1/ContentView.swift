d44f3d2122b2fe9df6f6c3ea3dc8ac3aa65346ad
armaan535kh <kh.armaan535@gmail.com>
2024-09-01 21:56:00 -0700

    Initial Commit

---
+++ Pro_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pro_1
+//
+//  Created by Armaan Khan  on 01/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pro_1/Pro_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_1App.swift
+//  Pro_1
+//
+//  Created by Armaan Khan  on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Pro_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
